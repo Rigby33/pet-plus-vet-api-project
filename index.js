0c9cff4105b9b69a49235261e5aa9cbec5bd4fd6
Catherine Lynn <catherineannlynn@gmail.com>
2017-12-07 06:19:49 -0500

    getting api to work

--- index.js
+++ index.js
@@ -1,36 +1,50 @@
-const petfinderApiUrl = 'http://api.petfinder.com/pet.find';
+const petfinderApiUrl = 'http://api.petfinder.com/pet.find?';
 const petfinderApiKey = '9efdec91ddde1fb83e7d7af7fb5f03ee';
 
-function getDataFromApi (pickAnimal, pickSize, pickSex, enterLocation, pickAge) {
-  const settings = {
-    url: petfinderApiUrl,
-    data: {
-      key: petfinderApiKey,
-      animal: `${pickAnimal}`,
-      size: `${pickSize}`,
-      sex: `${pickSex}`,
-      location: `${enterLocation}`,
-      age: `${pickAge}`,
-      format: 'json'
-    },
-		dataType: 'json',
-    type: 'GET',
-
+function getDataFromApi(pickAnimal, pickSize, pickSex, enterLocation, pickAge) {
+  const paramsObj = {
+    format: 'json',
+    key: petfinderApiKey,
+    animal: pickAnimal,
+    size: pickSize,
+    sex: pickSex,
+    location: enterLocation,
+    age: pickAge,
+    callback: '?'
   };
-  $.ajax(settings);
+  let queryParams = buildQueryString(paramsObj);
+  let url = petfinderApiUrl + queryParams;
+  $.getJSON(url, function (data) {
+    let thepets = data.petfinder.pets.pet;
+    $('.results').append(`<p>${thepets.name.$t}</p>`)
+  });
 }
 
-function searchForAPet () {
+function buildQueryString(myObject) {
+  let queryString = Object.keys(myObject).map( (e) => {
+      return e + '=' + myObject[e];
+  }
+    ).join('&');
+  return queryString;
+}
+
+function handlePets(pets) {
+  // do stuff with your pets object
+  for (let i = 0; i < pets.length; ++i) {
+    console.log(pets[i].name.$t);
+  }
+}
+
+function searchForAPet() {
   $('form').submit(function (event) {
     event.preventDefault();
-    const animal = $('.typeOfAnimal option').val();
-    const size = $('.sizeOfAnimal > option').val();
-    const sex = $('.sexOfAnimal > option').val();
+    const animal = $('.typeOfAnimal').val();
+    const size = $('.sizeOfAnimal').val();
+    const sex = $('.sexOfAnimal').val();
     const zipCode = $('.myZipCode').val();
-    const age = $('.agoOfAnimal > option').val();
-    console.log(getDataFromApi(animal, size, sex, zipCode, age));
+    const age = $('.agoOfAnimal').val();
+    getDataFromApi(animal, size, sex, zipCode, age)
   });
-  // console.log(getDataFromApi('cat', 'M', 'M', 46077, 'Young'))
 }
 
 $(searchForAPet);
